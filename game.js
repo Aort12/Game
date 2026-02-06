0b39de13274322ffad78a2a9d29581837fb3f90a
Okarun <venyadrochit228@gmail.com>
2026-02-06 13:58:37 +0200

    add some things

--- game.js
+++ game.js
@@ -4,4 +4,34 @@ function getComputerChoice() {
     return randomWords
     }
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
+
+
+function getHumanChoice(){
+    const choice = ["Rock", "Paper", "Scissors"];
+    const yourchoice = window.prompt('Your choice? You can choose Rock, Paper, Scissors').toLowerCase();
+    return yourchoice;
+}
+
+console.log(getHumanChoice());
+
+let humanScore 
+humanScore = '0';
+
+let computerScore
+computerScore = '0';
+
+alert(humanScore, computerScore);
+
+console.log(humanScore, computerScore);
+
+function playRound(HumanChoice, ComputerChoice) {
+    
+}
+
+console.log(playRound());
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
