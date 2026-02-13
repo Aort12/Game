05450a0f2c44f5697efebcba861c323c927a17c4
Okarun <venyadrochit228@gmail.com>
2026-02-13 17:04:00 +0200

    fixing functions: getHumanChoice, playRound, adding alert in function getComputerChoice.

--- game.js
+++ game.js
@@ -1,37 +1,75 @@
+
+
+
 function getComputerChoice() {
-    const words = ["Rock", "Paper", "Scissors"];
-    const randomWords = words[Math.floor(Math.random() * words.length)];
-    return randomWords
+    let words = ["rock", "paper", "scissors"];
+    let randomWords = words[Math.floor(Math.random() * words.length)];
+    alert (randomWords);
+    return randomWords;
     }
 
 console.log(getComputerChoice());
 
 
 function getHumanChoice(){
-    const choice = ["Rock", "Paper", "Scissors"];
-    const yourchoice = window.prompt('Your choice? You can choose Rock, Paper, Scissors').toLowerCase();
-    return yourchoice;
+    let choice = prompt('You can choose: paper, rock, scissors');
+    if (["rock", "paper", "scissors"].includes(choice)){
+        return choice;
+    }
 }
-
 console.log(getHumanChoice());
 
-let humanScore 
-humanScore = '0';
+let humanScore = 0;
+let computerScore = 0;
 
-let computerScore
-computerScore = '0';
 
-alert(humanScore, computerScore);
+function playRound() {
+    const ComputerChoice = getComputerChoice();
+    const HumanChoice = getHumanChoice();
 
-console.log(humanScore, computerScore);
+    if (
+        (HumanChoice == "rock" && ComputerChoice == "scissors") ||
+        (HumanChoice == "paper" && ComputerChoice == "rock") ||
+        (HumanChoice == "scissors" && ComputerChoice == "paper")) {
+            humanScore++;
+            alert('You win! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+        } else if(HumanChoice == ComputerChoice) {
+            alert('Its a tie! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+        } else {
+            computerScore++;
+            alert('You lose! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+        }
 
-function playRound(HumanChoice, ComputerChoice) {
-    
+
 }
+        
+    console.log(playRound())
 
-console.log(playRound());
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+    
+
+
+
+
+
+ 
+
+
+/*if (
+        (getHumanChoice == "rock" && getComputerChoice == "scissors") ||
+        (getHumanChoice == "paper" && getComputerChoice == "rock") ||
+        (getHumanChoice == "scissors" && getComputerChoice == "paper")) {
+            ++humanScore;
+            alert('You win! Your score:' + (humanScore),'Computer Score:' + (computerScore))
+        } else if(getHumanChoice == getComputerChoice) {
+            alert('Its a tie! Your score:' + (humanScore), 'Computer Score:' + (computerScore))
+        } else {
+            ++computerScore;
+            alert('You lose! Your score:' + (humanScore), 'Computer Score:' + (computerScore))
+        }
+    */
+
+
+
+
