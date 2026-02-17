960169cf8ff40db64f0f5c0c179e8e2c09f9c640
Okarun <venyadrochit228@gmail.com>
2026-02-17 17:20:43 +0200

    Changed function position and construction, fixing problems(infinity rounds, unusable human choice funcrtion). Game ready!

--- game.js
+++ game.js
@@ -1,59 +1,82 @@
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
+    function playRound() {
+        
 
-function getComputerChoice() {
-    let words = ["rock", "paper", "scissors"];
-    let randomWords = words[Math.floor(Math.random() * words.length)];
-    alert (randomWords);
-    return randomWords;
-    }
-
-console.log(getComputerChoice());
-
-
-function getHumanChoice(){
-    let choice = prompt('You can choose: paper, rock, scissors');
-    if (["rock", "paper", "scissors"].includes(choice)){
-        return choice;
-    }
-}
-console.log(getHumanChoice());
-
-let humanScore = 0;
-let computerScore = 0;
-
-
-function playRound() {
-    const ComputerChoice = getComputerChoice();
-    const HumanChoice = getHumanChoice();
-
-    if (
-        (HumanChoice == "rock" && ComputerChoice == "scissors") ||
-        (HumanChoice == "paper" && ComputerChoice == "rock") ||
-        (HumanChoice == "scissors" && ComputerChoice == "paper")) {
-            humanScore++;
-            alert('You win! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
-        } else if(HumanChoice == ComputerChoice) {
-            alert('Its a tie! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
-        } else {
-            computerScore++;
-            alert('You lose! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+        function getComputerChoice() {
+        let words = ["rock", "paper", "scissors"];
+        let randomWords = words[Math.floor(Math.random() * words.length)];
+        alert(randomWords);
+        return randomWords;
         }
 
 
-}
+        function getHumanChoice(){
+            let choice = prompt('You can choose: paper, rock, scissors')
+            
+            return choice.toLowerCase();
+        }
         
-    console.log(playRound())
+
+        const computerChoice = getComputerChoice();
+        const humanChoice = getHumanChoice();
+        
+        if (
+            (humanChoice == "rock" && computerChoice == "scissors") ||
+            (humanChoice == "paper" && computerChoice == "rock") ||
+            (humanChoice == "scissors" && computerChoice == "paper")) {
+                humanScore++;
+                alert('You win! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+            } else if(humanChoice == computerChoice) {
+                alert('Its a tie! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+            } else if (humanChoice != 'paper' && humanChoice != 'rock' && humanChoice != 'scissors'){
+                alert("You choice invalid element")
+            } else {
+                computerScore++;
+                alert('You lose! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+            } 
+        
+
+
+    }
 
 
 
+    let i = playRound();
+    
+    for (i=0; i<5; i++) {
+        
+
+    if (humanScore == 5){
+        alert('You win the game)')
+        break;
+        
+        
+        
+        } else if (computerScore == 5){
+            alert('You lose this game')
+            break; 
+        }
+        
+
+     
+     
+     playRound();
+    
+    }
+    
+    
     
 
+    
+}
+
+playGame();
 
 
-
-
- 
 
 
 /*if (
