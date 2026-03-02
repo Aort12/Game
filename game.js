a64daaf6481c0e978a2015431c2355140a6cb082
Okarun <venyadrochit228@gmail.com>
2026-03-02 14:04:48 +0200

    Remaked game(adden buttons and modified game logic)

--- game.js
+++ game.js
@@ -1,84 +1,60 @@
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
 
-    function playRound() {
-        
+document.getElementById('start').addEventListener('click', playGame);
 
-        function getComputerChoice() {
+function getComputerChoice() {
         let words = ["rock", "paper", "scissors"];
         let randomWords = words[Math.floor(Math.random() * words.length)];
-        alert(randomWords);
+        alert(randomWords)
         return randomWords;
-        }
+    }
 
-
-        function getHumanChoice(){
-            let choice = prompt('You can choose: paper, rock, scissors')
+    function playGame() {
+        function playRound(){
             
-            return choice.toLowerCase();
+            
+            
+
+            document.getElementById('rock').addEventListener('click', variants);
+            document.getElementById('paper').addEventListener('click', variants);
+            document.getElementById('scissors').addEventListener('click', variants);
+
+        function variants(event){
+            const computerChoice = getComputerChoice();
+            const humanChoice = event.target.id
+            if (
+                (humanChoice == "rock" && computerChoice == "scissors") ||
+                (humanChoice == "paper" && computerChoice == "rock") ||
+                (humanChoice == "scissors" && computerChoice == "paper")) {
+                    humanScore++;
+                    console.log('You win! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+                } else if(humanChoice == computerChoice) {
+                    console.log('Its a tie! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+                } else {
+                    computerScore++;
+                    console.log('You lose! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
+        
+                } 
+            
+            if (humanScore == 5){
+                    alert('You win the all game)');
+                    location.reload() 
+                } else if (computerScore == 5){
+                    alert('You lose the all game)');
+                    location.reload()
+                }
+                
+                }}
+            start.disabled = true;
+            playRound();
         }
+
+                
+
+            
+            
         
-
-        const computerChoice = getComputerChoice();
-        const humanChoice = getHumanChoice();
-        
-        if (
-            (humanChoice == "rock" && computerChoice == "scissors") ||
-            (humanChoice == "paper" && computerChoice == "rock") ||
-            (humanChoice == "scissors" && computerChoice == "paper")) {
-                humanScore++;
-                alert('You win! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
-            } else if(humanChoice == computerChoice) {
-                alert('Its a tie! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
-            } else if (humanChoice != 'paper' && humanChoice != 'rock' && humanChoice != 'scissors'){
-                alert("You choice invalid element")
-            } else {
-                computerScore++;
-                alert('You lose! Score: You - ' + humanScore + ', Computer Score - ' + computerScore);
-            } 
-        
-
-
-    }
-
-
-
-    let i = playRound();
-    
-    for (i=0; i<11; i++) {
-        
-
-    if (humanScore == 5){
-        alert('You win the game)');
-        break;
-        
-        
-        
-        } else if (computerScore == 5){
-            alert('You lose this game');
-            break; 
-        
-        } else if (i == 10){
-            alert('Game is end, try again');
-            break;
-        }
-     
-     
-     playRound();
-    
-    }
-    
-    
-    
-
-    
-}
-
-playGame();
-
-
-
 
 
